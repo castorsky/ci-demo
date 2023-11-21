51ecb6fbed7f1eccc96b9bb2f30a6075348331be
castorsky <csky57@gmail.com>
2023-11-21 21:49:32 +0300

    Update hello-world.py
    C-style string

--- hello-world.py
+++ hello-world.py
@@ -9,5 +9,5 @@ response = client.request('pool.ntp.org')
 testTime = time.strftime('%m/%d/%Y %H:%M', time.localtime(response.tx_time))
 question = os.getenv("CI", "Undefined")
 
-print(f"Hello, world! The time is {testTime}")
+print("Hello, world! The time is %s" % testTime)
 print(f"Hello, Slurm! It's running inside CI - {question}")
